5f85a03b7f03436c8797ce729d0d7c5a0570f588
smuthukumar <smuthukumar@vmware.com>
2022-03-17 00:19:34 +0530

    For shared/workload cluster. Marketplace downloading for ubuntu/photon naming conventions

--- scripts/workflows/ra_shared_cluster_upgrade.py
+++ scripts/workflows/ra_shared_cluster_upgrade.py
@@ -1,11 +1,12 @@
 import os
 import json
-from constants.constants import Paths
+from constants.constants import Paths, UpgradeVersions
 from lib.tkg_cli_client import TkgCliClient
 from model.run_config import RunConfig
 from util.logger_helper import LoggerHelper
 from workflows.cluster_common_workflow import ClusterCommonWorkflow
 import traceback
+from util.common_utils import downloadAndPushKubernetesOvaMarketPlace
 
 logger = LoggerHelper.get_logger(name='ra_shared_upgrade_workflow')
 
@@ -20,29 +21,53 @@ class RaSharedUpgradeWorkflow:
 
     def upgrade_workflow(self):
         try:
-            mgmt_cluster = self.jsonspec['tkgComponentSpec']['tkgMgmtComponents']['tkgMgmtClusterName']
-            cluster = self.jsonspec['tkgMgmtComponents']['tkgSharedserviceClusterName']
-            self.tanzu_client.login(cluster_name=mgmt_cluster)
-            if self.tanzu_client.tanzu_cluster_upgrade(cluster_name=cluster) is None:
-                msg = "Failed to upgrade {} cluster".format(cluster)
-                logger.error("Error: {}".format(msg))
-                raise Exception(msg)
 
-            if not self.tanzu_client.retriable_check_cluster_exists(cluster_name=cluster):
-                msg = f"Cluster: {cluster} not in running state"
-                logger.error(msg)
-                raise Exception(msg)
+            # Precheck for template.
 
-            logger.info("Checking for services status...")
-            cluster_status = self.tanzu_client.get_all_clusters()
-            shared_health = ClusterCommonWorkflow.check_cluster_health(cluster_status, cluster)
-            if shared_health == "UP":
-                msg = f"Shared Cluster {cluster} upgraded successfully"
-                logger.info(msg)
+            logger.info(("Checking if required template is already present"))
+            kubernetes_ova_os = \
+                self.jsonspec["tkgComponentSpec"]["tkgMgmtComponents"][
+                    "tkgMgmtBaseOs"]
+            kubernetes_ova_version = UpgradeVersions.KUBERNETES_OVA_LATEST_VERSION
+            down_status = downloadAndPushKubernetesOvaMarketPlace(self.jsonspec,
+                                                                  kubernetes_ova_version,
+                                                                  kubernetes_ova_os,
+                                                                  upgrade=True)
+            if down_status[0] is None:
+                logger.error(down_status[1])
+                d = {
+                    "responseType": "ERROR",
+                    "msg": down_status[1],
+                    "ERROR_CODE": 500
+                }
+                logger.error("Error: {}".format(json.dumps(d)))
+                msg = "Failed to download template..."
+                raise Exception(msg)
             else:
-                msg = f"Shared Cluster {cluster} failed to upgrade"
-                logger.error(msg)
-                raise Exception(msg)
+
+                mgmt_cluster = self.jsonspec['tkgComponentSpec']['tkgMgmtComponents']['tkgMgmtClusterName']
+                cluster = self.jsonspec['tkgMgmtComponents']['tkgSharedserviceClusterName']
+                self.tanzu_client.login(cluster_name=mgmt_cluster)
+                if self.tanzu_client.tanzu_cluster_upgrade(cluster_name=cluster) is None:
+                    msg = "Failed to upgrade {} cluster".format(cluster)
+                    logger.error("Error: {}".format(msg))
+                    raise Exception(msg)
+
+                if not self.tanzu_client.retriable_check_cluster_exists(cluster_name=cluster):
+                    msg = f"Cluster: {cluster} not in running state"
+                    logger.error(msg)
+                    raise Exception(msg)
+
+                logger.info("Checking for services status...")
+                cluster_status = self.tanzu_client.get_all_clusters()
+                shared_health = ClusterCommonWorkflow.check_cluster_health(cluster_status, cluster)
+                if shared_health == "UP":
+                    msg = f"Shared Cluster {cluster} upgraded successfully"
+                    logger.info(msg)
+                else:
+                    msg = f"Shared Cluster {cluster} failed to upgrade"
+                    logger.error(msg)
+                    raise Exception(msg)
 
         except Exception:
             logger.error("Error Encountered: {}".format(traceback.format_exc()))
