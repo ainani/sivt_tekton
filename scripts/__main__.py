91463c4806189f81e0e84dd505bb86c3f74263d2
Abhishek Inani <ainani@vmware.com>
2022-07-25 16:41:08 +0530

    renamed wld tasks

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -143,13 +143,13 @@ def wl_upgrade(ctx):
     run_config = load_run_config(ctx.obj["ROOT_DIR"])
     RaWorkloadUpgradeWorkflow(run_config).upgrade_workflow()
 
-@workload_clusters.command(name="tkgs-wl")
+@workload_clusters.command(name="tkgs-wld-setup")
 @click.pass_context
 def tkgs_workload(ctx):
     run_config = load_run_config(ctx.obj["ROOT_DIR"])
     RaWorkloadClusterWorkflow(run_config).create_workload()
 
-@workload_clusters.command(name="tkgs-ns")
+@workload_clusters.command(name="tkgs-wld-ns-setup")
 @click.pass_context
 def tkgs_namespace(ctx):
     run_config = load_run_config(ctx.obj["ROOT_DIR"])
