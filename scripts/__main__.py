309d363af91bf4c52eec56b36c11bab5039259d3
Abhishek Inani <ainani@vmware.com>
2022-07-27 21:22:42 +0530

    updated main fileg

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -160,7 +160,7 @@ def tkgs_namespace(ctx):
 def extns(ctx):
     ctx.ensure_object(dict)
 
-@cli.command(name="deploy")
+@extns.command(name="deploy")
 @click.pass_context
 def extns_deploy(ctx):
     run_config = load_run_config(ctx.obj["ROOT_DIR"])
