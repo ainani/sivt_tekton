cd37d8aea52fe0e402412c22cd48d3cd5979a33d
poojad4 <poojad4@vmware.com>
2022-08-07 11:37:16 +0530

    added plugin sync

--- scripts/workflows/ra_shared_cluster_workflow.py
+++ scripts/workflows/ra_shared_cluster_workflow.py
@@ -415,6 +415,10 @@ class RaSharedClusterWorkflow:
                 "ERROR_CODE": 500
             }
             return json.dumps(d), 500
+        #Init tanzu cli plugins
+        tanzu_init_cmd = "tanzu plugin sync"
+        command_status = self.rcmd.run_cmd_output(tanzu_init_cmd)
+        logger.debug("Tanzu plugin output: {}".format(command_status))
         podRunninng = ["tanzu", "cluster", "list"]
         command_status = runShellCommandAndReturnOutputAsList(podRunninng)
         if command_status[1] != 0:
